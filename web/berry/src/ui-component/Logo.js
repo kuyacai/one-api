6a328abcba633227f6a956e587e64512b8ba6024
kuyacai <24796839@qq.com>
2024-08-23 00:13:23 +0800

    update logo

--- web/berry/src/ui-component/Logo.js
+++ web/berry/src/ui-component/Logo.js
@@ -1,6 +1,6 @@
 // material-ui
-import logoLight from 'assets/images/logo.svg';
-import logoDark from 'assets/images/logo-white.svg';
+import logoLight from 'assets/images/nine-one-400-1800.png';
+import logoDark from 'assets/images/nine-one-400-1800.png';
 import { useSelector } from 'react-redux';
 import { useTheme } from '@mui/material/styles';
 
@@ -19,7 +19,7 @@ const Logo = () => {
   const theme = useTheme();
   const logo = theme.palette.mode === 'light' ? logoLight : logoDark;
 
-  return <img src={siteInfo.logo || logo} alt={siteInfo.system_name} height="50" />;
+  return <img src={siteInfo.logo || logo} alt={siteInfo.system_name} height="40" />;
 };
 
 export default Logo;
