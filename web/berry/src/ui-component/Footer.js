093e36f1f2c7a2362eb8df22d50bf8e66787e05b
kuyacai <24796839@qq.com>
2024-08-23 09:48:53 +0800

    Update footer information

--- web/berry/src/ui-component/Footer.js
+++ web/berry/src/ui-component/Footer.js
@@ -2,13 +2,13 @@
 import { Link, Container, Box } from '@mui/material';
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { useTranslation } from 'react-i18next';
+//import { useTranslation } from 'react-i18next';
 
 // ==============================|| FOOTER - AUTHENTICATION 2 & 3 ||============================== //
 
 const Footer = () => {
   const siteInfo = useSelector((state) => state.siteInfo);
-  const { t } = useTranslation();
+    // const { t } = useTranslation();
   return (
     <Container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '64px' }}>
       <Box sx={{ textAlign: 'center' }}>
@@ -19,15 +19,15 @@ const Footer = () => {
             <Link href="https://github.com/songquanpeng/one-api" target="_blank">
               {siteInfo.system_name} {process.env.REACT_APP_VERSION}{' '}
             </Link>
-            由{' '}
+            built by{' '}
             <Link href="https://github.com/songquanpeng" target="_blank">
               JustSong
             </Link>{' '}
-            构建，主题 berry 来自{' '}
+            with the berry theme by{' '}
             <Link href="https://github.com/MartialBE" target="_blank">
               MartialBE
-            </Link>{' '}，源代码遵循
-            <Link href="https://opensource.org/licenses/mit-license.php"> MIT 协议</Link>
+            </Link>{' '}. The source code is licensed under the
+            <Link href="https://opensource.org/licenses/mit-license.php"> MIT License</Link>
           </>
         )}
       </Box>
